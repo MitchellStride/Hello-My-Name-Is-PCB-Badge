c42af9a9e28ea9419e1d856f2ac0f104f125bbc0
Mitchell Stride <mitchellcstride@gmail.com>
2022-05-10 22:26:43 -0700

    sw transfer

--- hello_my_name_is/software/unit_tests/oled_simple.py
+++ hello_my_name_is/software/unit_tests/oled_simple.py
@@ -1,8 +1,10 @@
-from machine import Pin, I2C
+import machine
 from ssd1306 import SSD1306_I2C
 
-i2c=I2C(0,sda=Pin(27), scl=Pin(28), freq=400000)  #fix pins
-oled = SSD1306_I2C(128, 64, i2c)
+oled_rst = machine.Pin(15, machine.Pin.OUT)
+oled_rst.high()
+i2c0 = machine.I2C(0, sda=machine.Pin(16), scl=machine.Pin(17), freq=400000)
+oled = SSD1306_I2C(128, 64, i2c0)
 
 oled.text("Mitchell Stride", 0, 0)
 oled.show()
